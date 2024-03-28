5eaf47f3e4d45c076599a0ecfdbaa51117eead4b
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-03-28 19:25:59 +0700

    Cout arr

--- QuickSort_0050.cpp
+++ QuickSort_0050.cpp
@@ -96,4 +96,9 @@ void display() {
 	cout << "\n-------------------" << endl;
 	cout << "Sorted Array" << endl;
 	cout << "---------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
 }
