0a9e05c85d27b719e0e66013033a97e31e44fc88
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-03-28 19:24:25 +0700

    Sorted array

--- QuickSort_0050.cpp
+++ QuickSort_0050.cpp
@@ -86,4 +86,14 @@ void q_short(int low, int high)
 
 	//recursive call to sort the right sub array
 	q_short(j + 1, high); //step 13
+
+
+
+}
+
+
+void display() {
+	cout << "\n-------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "---------------------" << endl;
 }
